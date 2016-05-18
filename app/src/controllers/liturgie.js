4483ed57d9f612f0513bc791437c2f280b884426
Harm Doedens <doedens.h@tkppensioen.nl>
2016-05-18 15:10:58 +0200

    added type-ahead-on-select

--- app/src/controllers/liturgie.js
+++ app/src/controllers/liturgie.js
@@ -3,11 +3,11 @@ function liturgie($scope) {
 	$scope.onderdelen = ['']
 
   $scope.manageInputs = function(index, regel) {
-		console.log(index + ": " + regel)
-		console.log("aantal regels: " + $scope.onderdelen.length)
-		console.log("$scope.onderdelen[index-1]: " + $scope.onderdelen[index-1])
-		console.log("regel: " + regel)
-		console.log("$scope.onderdelen.length -2: " + ($scope.onderdelen.length - 2))
+		// console.log(index + ": " + regel)
+		// console.log("aantal regels: " + $scope.onderdelen.length)
+		// console.log("$scope.onderdelen[index-1]: " + $scope.onderdelen[index-1])
+		// console.log("regel: " + regel)
+		// console.log("$scope.onderdelen.length -2: " + ($scope.onderdelen.length - 2))
 		
 		// lege input toevoegen
 		if(index == $scope.onderdelen.length -1 && regel != '') {
@@ -18,5 +18,7 @@ function liturgie($scope) {
 		if(index == $scope.onderdelen.length -2 && regel == '' && $scope.onderdelen[$scope.onderdelen.length-1] == '') {
 			$scope.onderdelen.pop()
 		}
+		
+		$scope.onderdelen[index] = regel;
   }
 }
