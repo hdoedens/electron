a43b216f05314b8184b0a3409ac3c2bfe7feec99
Harm Doedens <doedens.h@tkppensioen.nl>
2016-05-26 16:49:57 +0200

    fixed validity directive

--- app/js/liturgie.js
+++ app/js/liturgie.js
@@ -1,30 +1,25 @@
 angular.module('liturgie', [])
 	.controller('LiturgieController', function liturgie($scope) {
-		
+
 		$scope.onderdelen = ['']
-	
+
 		$scope.manageInputs = function(index, regel) {
-			// console.log(index + ": " + regel)
-			// console.log("aantal regels: " + $scope.onderdelen.length)
-			// console.log("$scope.onderdelen[index-1]: " + $scope.onderdelen[index-1])
-			// console.log("regel: " + regel)
-			// console.log("$scope.onderdelen.length -2: " + ($scope.onderdelen.length - 2))
-			
+
 			// lege input toevoegen
 			if(index == $scope.onderdelen.length -1 && regel != '') {
 				$scope.onderdelen.push('')
 			}
-			
+
 			// lege input verwijderen
 			if(index == $scope.onderdelen.length -2 && regel == '' && $scope.onderdelen[$scope.onderdelen.length-1] == '') {
 				$scope.onderdelen.pop()
 			}
-			
+
 			$scope.onderdelen[index] = regel;
 		}
-		
+
 	})
-	.directive('show-validity', function() {
+	.directive('showValidity', function() {
 		return {
 			restrict: 'A',
 			link: function(scope, el, attrs, formCtrl) {
@@ -35,11 +30,12 @@ angular.module('liturgie', [])
 				// get the name on the text box so we know the property to check
 				// on the form controller
 				var inputName = inputNgEl.attr('name');
-				inputNgEl.pattern = "/foo/"
 
 				// apply the has-error class as soon as the user changes the text box contents
-				inputNgEl.bind('change', function() {
-					el.toggleClass('has-error', formCtrl[inputName].$invalid);
+				inputNgEl.bind('keyup', function() {
+					var valid = inputNgEl.val().match(/gezang/)
+					el.toggleClass('has-error', !valid && inputNgEl.val() != '');
+					el.toggleClass('has-success', valid);
 				})
 			}
 		}
