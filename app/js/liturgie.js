94435ee8615691c66eff2fc5cb511f953d1dbf8e
Harm Doedens <doedens.h@tkppensioen.nl>
2016-06-02 16:20:52 +0200

    added live preview

--- app/js/liturgie.js
+++ app/js/liturgie.js
@@ -8,17 +8,21 @@ angular.module('liturgie', [])
             }
         });
 
-        $scope.getTekst = function (index) {
-            console.log('haal tekst op voor onderdeel: ' + index);
+        $scope.setTextOfItem = function (index) {
+            $scope.onderdelen[index].tekst = $scope.onderdelen[index].regel
         }
 
-        $scope.onderdelen = [{regel:"", tekst:"foobar"}]
+        $scope.unsetTextOfItem = function (index) {
+            $scope.onderdelen[index].tekst = ''
+        }
+
+        $scope.onderdelen = [{regel:"", tekst:""}]
 
         $scope.manageInputs = function (index, onderdeel) {
 
             // lege input toevoegen
             if (index == $scope.onderdelen.length - 1 && onderdeel.regel != '') {
-                $scope.onderdelen.push({regel:"", tekst:"foobar"})
+                $scope.onderdelen.push({regel:"", tekst:""})
             }
 
             // lege input verwijderen
@@ -47,10 +51,13 @@ angular.module('liturgie', [])
                     for (n in scope.validationRules) {
                         valid = scope.validationRules[n].test(element.val())
                         if (valid) {
-                            scope.getTekst(attrs.id);
                             break;
                         }
                     }
+                    if(valid)
+                        scope.setTextOfItem(attrs.id);
+                    else
+                        scope.unsetTextOfItem(attrs.id);
                     ngModel.valid = valid;
                     ngModel.invalid = !valid;
 
