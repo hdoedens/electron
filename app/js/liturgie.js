cd686e5bd182ca33b397d2709304d7e695fb7175
Harm Doedens <hdoedens@gmail.com>
2016-06-03 22:12:25 +0200

    added more validation

--- app/js/liturgie.js
+++ app/js/liturgie.js
@@ -1,71 +1,73 @@
 angular.module('liturgie', [])
-    .controller('LiturgieController', function ($scope, $http) {
+	.controller('LiturgieController', function ($scope, $http) {
 
-        $scope.validationRules = []
-        $http.get('./resources/validation.json').then(function (response) {
-            for (n in response.data) {
-                $scope.validationRules.push(new RegExp(response.data[n]));
-            }
-        });
-        
-        $scope.setOnderdeelDetails = function(index) {
-            $scope.onderdelen[index].tekst = $scope.onderdelen[index].regel
-            $scope.onderdelen[index].icon = "fa-music"
-        }
+		$scope.validationRules = []
+		$http.get('./resources/validation.json').then(function (response) {
+			for (n in response.data) {
+				for (i in response.data[n].regexen) {
+					$scope.validationRules.push(new RegExp(response.data[n].regexen[i]));
+				}
+			}
+		});
 
-        $scope.clearOnderdeelDetails = function (index) {
-            $scope.onderdelen[index].tekst = ''
-            $scope.onderdelen[index].icon = 'fa-question'
-        }
+		$scope.setOnderdeelDetails = function (index) {
+			$scope.onderdelen[index].tekst = $scope.onderdelen[index].regel
+			$scope.onderdelen[index].icon = 'fa-music'
+		}
 
-        $scope.onderdelen = [{regel:"", tekst:"", icon:"fa-question"}]
+		$scope.clearOnderdeelDetails = function (index) {
+			$scope.onderdelen[index].tekst = ''
+			$scope.onderdelen[index].icon = 'fa-question'
+		}
 
-        $scope.manageInputs = function (index, onderdeel) {
+		$scope.onderdelen = [{ regel: "", tekst: "", icon: "fa-question" }]
 
-            // lege input toevoegen
-            if (index == $scope.onderdelen.length - 1 && onderdeel.regel != '') {
-                $scope.onderdelen.push({regel:"", tekst:"", icon:"fa-question"})
-            }
+		$scope.manageInputs = function (index, onderdeel) {
 
-            // lege input verwijderen
-            if (index == $scope.onderdelen.length - 2 && onderdeel.regel == '' && $scope.onderdelen[$scope.onderdelen.length - 1] == '') {
-                $scope.onderdelen.pop()
-            }
+			// lege input toevoegen
+			if (index == $scope.onderdelen.length - 1 && onderdeel.regel != '') {
+				$scope.onderdelen.push({ regel: "", tekst: "", icon: "fa-question" })
+			}
 
-            $scope.onderdelen[index].regel = onderdeel.regel;
-        }
+			// lege input verwijderen
+			if (index == $scope.onderdelen.length - 2 && onderdeel.regel == '' && $scope.onderdelen[$scope.onderdelen.length - 1] == '') {
+				$scope.onderdelen.pop()
+			}
 
-    })
-    .directive('testValidity', function () {
-        return {
-            restrict: 'A',
-            require: '?ngModel',
-            link: function (scope, element, attrs, ngModel) {
+			$scope.onderdelen[index].regel = onderdeel.regel;
+		}
 
-                element.on('blur keyup change', function () {
-                    scope.$evalAsync(read);
-                });
-                read(); // initialize
+	})
+	.directive('testValidity', function () {
+		return {
+			restrict: 'A',
+			require: '?ngModel',
+			link: function (scope, element, attrs, ngModel) {
 
-                // Write data to the model
-                function read() {
-                    var valid = false
-                    for (n in scope.validationRules) {
-                        valid = scope.validationRules[n].test(element.val())
-                        if (valid) {
-                            break;
-                        }
-                    }
-                    if(valid)
-                        scope.setOnderdeelDetails(attrs.id);
-                    else
-                        scope.clearOnderdeelDetails(attrs.id);
-                    ngModel.valid = valid;
-                    ngModel.invalid = !valid;
+				element.on('blur keyup change', function () {
+					scope.$evalAsync(read);
+				});
+				read(); // initialize
 
-                    element.parent().toggleClass('has-error', !valid && element.val() != '');
-                    element.parent().toggleClass('has-success', valid);
-                }
-            }
-        }
-    })
+				// Write data to the model
+				function read() {
+					var valid = false
+					for (n in scope.validationRules) {
+						valid = scope.validationRules[n].test(element.val())
+						if (valid) {
+							break;
+						}
+					}
+					if (valid)
+						scope.setOnderdeelDetails(attrs.id);
+					else
+						scope.clearOnderdeelDetails(attrs.id);
+					ngModel.valid = valid;
+					ngModel.invalid = !valid;
+
+					element.parent().toggleClass('has-error', !valid && element.val() != '');
+					element.parent().toggleClass('has-success', valid);
+				}
+			}
+		}
+	})
