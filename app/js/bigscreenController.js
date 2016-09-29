0b7b331accfa97f406e21ebe4b85aa076f4e7e3d
Doedens <Harm.Doedens@ordina.nl>
2016-09-29 20:32:34 +0200

    choose a regex and improve more

--- app/js/bigscreenController.js
+++ app/js/bigscreenController.js
@@ -12,16 +12,34 @@ liedbase.controller('BigscreenController', function ($sce, $scope, log, Liturgie
   })
 
   var formatTitle = function(title, highlight) {
-    var regex = new RegExp("(.*[: ])("+highlight+")[^0-9](.*)")
+    var regex = new RegExp("(.*[:] ?[0-9, ]*)[^0-9]("+highlight+")[^0-9]?(.*)")
+    var regex = new RegExp("(.*[:]*[^0-9])("+highlight+")([^0-9]?.*)")
     var match = regex.exec(title)
-    log.info(match)
-    // return title.replace(new RegExp(highlight), "<b>"+highlight+"</b>")
     var newTitle = match[1]+" <b>"+highlight+"</b>"
     if(match[3] != '')
-      return newTitle+", "+match[3]
+      return newTitle+","+match[3]
     else
       return newTitle
   }
+
+  console.log('1 levenslied 8: 1')
+  console.log(formatTitle('levenslied 8: 1', '1'))
+  console.log('1 levenslied 8: 1, 3')
+  console.log(formatTitle('levenslied 8: 1, 3', '1'))
+  console.log('3 levenslied 8: 1, 3')
+  console.log(formatTitle('levenslied 8: 1, 3', '3'))
+  console.log('3 levenslied 8: 1, 3, 5')
+  console.log(formatTitle('levenslied 8: 1, 3, 5', '3'))
+  console.log('1 levenslied 8: 1, 3, 5')
+  console.log(formatTitle('levenslied 8: 1, 3, 5', '1'))
+  console.log('5 levenslied 8: 1, 3, 5')
+  console.log(formatTitle('levenslied 8: 1, 3, 5', '5'))
+  console.log('1 levenslied 8: 1, 3, 5, 12')
+  console.log(formatTitle('levenslied 8: 1, 3, 5, 12', '1'))
+  console.log('3 levenslied 8: 1, 3, 5, 12')
+  console.log(formatTitle('levenslied 8: 1, 3, 5, 12', '3'))
+  console.log('12 levenslied 8: 1, 3, 5, 12')
+  console.log(formatTitle('levenslied 8: 1, 3, 5, 12', '12'))
 
   $scope.getHtml = function(data) {
     return $sce.trustAsHtml(data)
