afdb3ec59b548ea1dc081d919d59d54cc2e8dda2
Harm Doedens <doedens.h@tkppensioen.nl>
2017-02-14 16:36:58 +0100

    improved parsing

--- app/js/grabbers/bijbelController.js
+++ app/js/grabbers/bijbelController.js
@@ -4,13 +4,26 @@ liedbase.controller('BijbelController', function ($sce, $scope, $http, log, dbFa
     $scope.grab = function () {
 
         $http.get('assets/bible/BGT/genesis.txt').success(function(data){
-            data.split().foreach(function(line) {
+            var lines = data.split(/\r?\n/g)
+            dbLine = ""
+            for(var i=0; i<lines.length; i++) {
+                var line = lines[i];
                 if(line == "")
-                    return;
-                if(line.startsWith("="))
-                    return;
-                console.log(line);
-            });
+                    continue;
+                if(line.startsWith("=")) {
+                    dbLine += line.substring(1) + "\n"
+                    continue;
+                } 
+                if(line.startsWith("#")) {
+                    dbLine += line.substring(1) + ' '
+                    continue;
+                }
+
+                dbLine += line
+                console.log(dbLine);
+                dbLine = "";
+            
+            }
         });
 
     }
